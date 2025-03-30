9826cf6f9090eaa63c71cf68c64f25963def4a3a
Pooya Fekri <pooyafekri79@gmail.com>
2025-03-30 12:31:00 +0330

    perf(bots): Improve bots concurrency performance

--- zexporta/bots/transfer_test_token/main.py
+++ zexporta/bots/transfer_test_token/main.py
@@ -4,7 +4,9 @@ import logging.config
 
 from clients.evm.client import compute_create2_address, get_evm_async_client
 from eth_account.signers.local import LocalAccount
+from hexbytes import HexBytes
 from web3 import AsyncWeb3
+from web3.types import Nonce, TxReceipt
 
 from zexporta.bots.custom_types import BotToken
 from zexporta.bots.transfer_test_token.config import (
@@ -40,13 +42,42 @@ async def _get_last_user_id() -> UserId | None:
 
 async def _send_deposits(
     w3: AsyncWeb3,
-    test_tokens: list[BotToken],
+    test_token: BotToken,
     account: LocalAccount,
     user_address: ChecksumAddress,
     logger: logging.Logger | ChainLoggerAdapter,
-):
-    for test_token in test_tokens:
-        await send_deposit(w3, test_token, account, user_address, logger)
+    nonce: Nonce,
+) -> HexBytes:
+    return await send_deposit(w3, test_token, account, user_address, logger, nonce, False)
+
+
+async def _send_token_to_user_id(
+    w3: AsyncWeb3,
+    test_token: BotToken,
+    user_id: UserId,
+    account: LocalAccount,
+    logger: ChainLoggerAdapter,
+    nonce: Nonce,
+) -> HexBytes:
+    logger.info(f"Initiate transferring to user with id: {user_id}")
+
+    user_address = compute_create2_address(
+        salt=user_id,
+    )
+
+    logger.info(f"Deposit address for user with id: {user_id} is {user_address}")
+
+    tx_hash = await _send_deposits(
+        w3, test_token, account=account, user_address=user_address, logger=logger, nonce=nonce
+    )
+    logger.info(f"Transferring to user with id: {user_id} is completed")
+    return tx_hash
+
+
+async def check_transactions_receipt(w3: AsyncWeb3, txs_hash: list[HexBytes]) -> list[TxReceipt | BaseException]:
+    tasks = [asyncio.create_task(w3.eth.wait_for_transaction_receipt(tx_hash)) for tx_hash in txs_hash]
+    tx_receipts = await asyncio.gather(*tasks, return_exceptions=True)
+    return tx_receipts
 
 
 async def transfer_test_tokens(chain: EVMConfig):
@@ -55,35 +86,32 @@ async def transfer_test_tokens(chain: EVMConfig):
     account = w3.eth.account.from_key(HOLDER_PRIVATE_KEY)
     test_tokens = [token for token in TEST_TOKENS if token.chain_symbol == chain.chain_symbol]
     if len(test_tokens) == 0:
-        _logger.error("No token for transfer found")
-
+        _logger.warning("No token for transfer found")
+        return
     while True:
+        nonce = await w3.eth.get_transaction_count(account.address, "pending")
         last_transferred_user_id = await get_last_transferred_id(chain.chain_symbol) or 0
 
         last_user_id = await _get_last_user_id() or 0
 
         if last_transferred_user_id == last_user_id:
             _logger.info("There is no new user to transfer to")
-            await asyncio.sleep(chain.delay)
+            await asyncio.sleep(60)
+            continue
 
-        for id in range(last_transferred_user_id + 1, last_user_id + 1):
-            _logger.info(f"Initiate transferring to user with id: {id}")
-
-            user_address = compute_create2_address(
-                salt=id,
+        broadcast_tasks = []
+        for id_ in range(last_transferred_user_id + 1, min(last_transferred_user_id + 15, last_user_id + 1)):
+            broadcast_tasks.append(
+                asyncio.create_task(
+                    _send_token_to_user_id(
+                        w3, test_tokens[0], user_id=id_, account=account, logger=_logger, nonce=Nonce(nonce)
+                    )
+                )
             )
-
-            _logger.info(f"Deposit address for user with id: {id} is {user_address}")
-
-            await _send_deposits(
-                w3,
-                test_tokens,
-                account=account,
-                user_address=user_address,
-                logger=_logger,
-            )
-            await upsert_last_transferred_id(chain.chain_symbol, id)
-            _logger.info(f"Transferring to user with id: {id} is completed")
+            nonce += 1
+        txs_hash = await asyncio.gather(*broadcast_tasks)
+        _ = await check_transactions_receipt(w3, txs_hash)
+        await upsert_last_transferred_id(chain.chain_symbol, len(txs_hash) + last_transferred_user_id)
 
 
 async def main():
