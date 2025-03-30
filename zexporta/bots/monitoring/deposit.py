0cb7135ac31f571a8b6caf462406a1bfd9176a58
Pooya Fekri <pooyafekri79@gmail.com>
2025-03-30 12:31:00 +0330

    perf(bots): Improve bots concurrency performance

--- zexporta/bots/monitoring/deposit.py
+++ zexporta/bots/monitoring/deposit.py
@@ -10,19 +10,23 @@ from zexporta.bots.monitoring.config import (
     TEST_USER_ID,
     WITHDRAWER_PRIVATE_KEY,
 )
-from zexporta.bots.utils.deposit import send_deposit, wait_for_transaction_receipt
+from zexporta.bots.utils.deposit import send_deposit
 from zexporta.custom_types import EVMConfig, UserId
 from zexporta.utils.logger import ChainLoggerAdapter
 from zexporta.utils.zex_api import get_user_asset
+
+from ..custom_types import BotToken
 
 
 class DepositError(Exception):
     "raise when deposit was not successful"
 
 
-async def get_user_balance(client: httpx.AsyncClient, user_id: UserId, symbol: str) -> Decimal:
+async def get_user_balance(client: httpx.AsyncClient, user_id: UserId, token: BotToken) -> Decimal:
     balance = [
-        user_asset.free for user_asset in (await get_user_asset(client, user_id=user_id)) if user_asset.asset == symbol
+        user_asset.free
+        for user_asset in (await get_user_asset(client, user_id=user_id))
+        if user_asset.asset == token.symbol
     ]
     if len(balance) == 0:
         return Decimal(0)
@@ -40,17 +44,19 @@ async def monitor_deposit(async_client: httpx.AsyncClient, chain: EVMConfig, log
 
     w3 = get_evm_async_client(chain, logger).client
     account = w3.eth.account.from_key(WITHDRAWER_PRIVATE_KEY)
-    balance_before = await get_user_balance(async_client, TEST_USER_ID, monitoring_token.symbol)
+    balance_before = await get_user_balance(async_client, TEST_USER_ID, monitoring_token)
     logger.info(f"Balance before deposit: {balance_before}")
-
+    nonce = await w3.eth.get_transaction_count(account.address, "pending")
     tx_hash = await send_deposit(
         w3,
         monitoring_token,
         account=account,
         user_address=test_user_address,
         logger=logger,
+        nonce=nonce,
+        wait_for_receipt=False,
     )
-    receipt = await wait_for_transaction_receipt(w3, tx_hash, logger)
+    receipt = await w3.eth.wait_for_transaction_receipt(tx_hash)
     if receipt and receipt["status"] == 1:
         logger.info("Transaction successful.")
     else:
@@ -58,7 +64,7 @@ async def monitor_deposit(async_client: httpx.AsyncClient, chain: EVMConfig, log
 
     await asyncio.sleep(60)  # wait until deposit store in zex
 
-    balance_after = await get_user_balance(async_client, TEST_USER_ID, monitoring_token.symbol)
+    balance_after = await get_user_balance(async_client, TEST_USER_ID, monitoring_token)
     logger.info(f"Balance after deposit: {balance_after}")
 
     # Check if balance increased
--- zexporta/bots/utils/deposit.py
+++ zexporta/bots/utils/deposit.py
@@ -1,9 +1,10 @@
-import asyncio
 import logging
 import logging.config
 
 from eth_account.signers.local import LocalAccount
+from hexbytes import HexBytes
 from web3 import AsyncWeb3
+from web3.types import Nonce
 
 from zexporta.bots.custom_types import BotToken
 from zexporta.custom_types import ChecksumAddress
@@ -17,25 +18,18 @@ async def send_deposit(
     account: LocalAccount,
     user_address: ChecksumAddress,
     logger: logging.Logger | ChainLoggerAdapter,
-):
+    nonce: Nonce,
+    wait_for_receipt: bool,
+) -> HexBytes:
     ERC20_token = w3.eth.contract(address=monitoring_token.address, abi=ERC20_ABI)
-    nonce = await w3.eth.get_transaction_count(account.address, "pending")
     tx = await ERC20_token.functions.transfer(user_address, monitoring_token.amount).build_transaction(
         {"from": account.address, "nonce": nonce}
     )
     signed_tx = account.sign_transaction(tx)
     tx_hash = await w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    await w3.eth.wait_for_transaction_receipt(tx_hash)
     logger.info(f"Method called successfully. Transaction Hash: {tx_hash.hex()}")
+
+    if wait_for_receipt:
+        tx_receipt = await w3.eth.wait_for_transaction_receipt(tx_hash)
+        logger.info(f"Tx receipt is {tx_receipt}")
     return tx_hash
-
-
-async def wait_for_transaction_receipt(w3, tx_hash, logger):
-    while True:
-        try:
-            receipt = await w3.eth.get_transaction_receipt(tx_hash)
-            if receipt:
-                return receipt
-        except Exception as e:
-            logger.debug(f"Waiting for transaction receipt: {e}")
-        await asyncio.sleep(5)
